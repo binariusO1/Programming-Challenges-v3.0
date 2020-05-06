8b777e5313a9773b3b26e95baf8344a509c1970a
binariusO1 <54156262+binariusO1@users.noreply.github.com>
2020-05-06 23:41:05 +0200

    Add files via upload

---
+++ 003 - FizzBuzz (python 3.7)/05 - FizzBuzz.py
@@ -0,0 +1,17 @@
+# programming challenge
+# 05 FizzBuzz
+# python 3.7
+# binariusO1
+# 
+a=0
+while a<100:
+    i=int(input())
+    s=""
+    t=""
+    if i%5==0:
+        s="FIZZ"
+    if i%3==0:
+        t="BUZZ"
+    if i%3 and i%5:
+        print(i,end='')
+    print(s+t)
