f8d7812768842aa86fa1a58e69ec8e7015fdb6c6
binariusO1 <54156262+binariusO1@users.noreply.github.com>
2020-05-07 13:12:56 +0200

    Add files via upload

---
+++ 018 - Dynamic multidimensional array (C++)/Dynamic multidimensional array.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	unsigned width = 20;
+	unsigned height = 20;
+
+	int** mArray;
+
+	for (int i = 0; i < height; ++i)
+		mArray[i] = new int[width];
+
+	for (int i = 0; i < height; i++) {
+		for (int j = 0; j < width; j++) {
+			mArray[i][j] = 1;
+		}
+	}
+}
