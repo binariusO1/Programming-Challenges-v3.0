e1d5f7359546cec424908f4e359d65ef775bb283
binariusO1 <54156262+binariusO1@users.noreply.github.com>
2020-05-07 14:08:20 +0200

    Add files via upload

---
+++ 146 - Natural string order sorting/Natural string order sorting.cpp
@@ -0,0 +1,20 @@
+// Natural string order sorting (a1,a2,a10,a20 instead of a1,a10,a2,a20)
+// Natural sorting
+bool CprStringNatural(string& a, string& b)
+{
+	if ((a.size() < b.size()))
+		return 1;
+	else if ((a.size() > b.size()))
+		return 0;
+	else
+	{
+		for (int i = 0; i < a.size(); i++)
+		{
+			if (a[i] < b[i])
+				return 1;
+			if (a[i] > b[i])
+				return 0;
+		}
+	}
+	return 0;
+}
