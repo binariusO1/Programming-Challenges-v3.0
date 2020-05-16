76488fad828b580d15f1bcd916ddd9bb29205fee
binariusO1 <54156262+binariusO1@users.noreply.github.com>
2020-05-16 13:43:49 +0200

    Add files via upload

---
+++ 115 - Sine wave generator from pseudorandom numbers  (python 3.7, turtle)/Sine wave generator.py
@@ -0,0 +1,27 @@
+import math
+import turtle
+import random
+
+screen = turtle.Screen()
+screen.setup(width=1000, height=500)
+
+sin = turtle.Turtle()
+sin.penup()
+sin.setpos(500,0)
+sin.pendown()
+sin.setpos(-500,0)
+sin.speed(0)
+w = 360*6
+k = 50
+s = 0
+f = 0
+kat = random.randint(45, 180)
+
+for angle in range(w):
+    y = (math.sin(math.radians(angle))+math.sin(math.radians(angle*(f+1)))/(f+1))
+    sin.setpos(-500+(angle/2), y*float(k))
+    k += float(s*0.1*(f/2))
+    if angle%180 == 0:
+        s = random.randint(-1,1)
+        f = random.randint(0, 10)
+screen.bye()
