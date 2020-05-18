4f4a0a62c674b8f5346430701502baac8e33ae86
binariusO1 <54156262+binariusO1@users.noreply.github.com>
2020-05-18 15:26:55 +0200

    Update Quine C++.cpp

--- 009 - Quine/Quine C++.cpp
+++ 009 - Quine/Quine C++.cpp
@@ -3,14 +3,32 @@ Programing challenge
 2020-05-18 binariusO1
 Quine C++4)
 */
-#include <iostream>
+#include<iostream>
 using namespace std;
-int main()
+int main() 
 {
-	cout << "#include <iostream>" << endl;
-	cout << "using namespace std;" << endl;
-	cout << "int main()" << endl;
-	cout << "{" << endl;
-	cout << "cout<<" << '"' << "Hello World" << '"' <<"endl;" << endl;
-	cout << "}" << endl;
+char sep = 34;
+string word[] = {
+	"    ",
+	"#include<iostream>",
+	"using namespace std;",
+	"int main()",
+	"{",
+	"char sep = 34;",
+	"string word[] = {",
+	"}",
+	"for(int i = 1; i <= 6; i++)",
+	"cout << word[i] << endl;",
+	"for(int i = 0; i <= 14; i++)",
+	"cout << word[0] + sep + word[i] + sep + ',' << endl;",
+	"for(int i = 8; i <= 14; i++)",
+	"cout << word[i] << endl;",
+	"}"
+};
+for (int i = 1; i <= 6; i++)
+cout << word[i] << endl;
+for (int i = 0; i <= 14; i++)
+cout << word[0] + sep + word[i] + sep + ',' << endl;
+for (int i = 8; i <= 14; i++)
+cout << word[i] << endl;
 }
